4a8849f603bcdb87419071a159ee8ebbe5730fe4
Alexander <alexander.va@hotmail.com>
2025-07-28 14:01:57 -0600

    fix: rename CleanExpiredReports to CleanPurchaseReceipts

--- app/Console/Commands/CleanPurchaseReceipts.php
+++ app/Console/Commands/CleanPurchaseReceipts.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 
-class CleanExpiredReports extends Command
+class CleanPurchaseReceipts extends Command
 {
     /**
      * The name and signature of the console command.
